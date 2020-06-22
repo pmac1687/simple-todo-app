95fa624a1a34c96199a2b7f799667fbcc56ffdbb
pat <pmac1687@outlook.com>
2020-06-22 11:59:28 -0700

    Updates

---
+++ precache-manifest.ecbb88f5038b66fa60c9ea8dba6bf7cc.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "519a0f4cae390929863767aaf335abac",
+    "url": "/simple-todo-app/index.html"
+  },
+  {
+    "revision": "41a4eabb546362dfb98a",
+    "url": "/simple-todo-app/static/css/main.1f1207c6.chunk.css"
+  },
+  {
+    "revision": "e1c993e3070ff6d419bc",
+    "url": "/simple-todo-app/static/js/2.aadc151e.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/simple-todo-app/static/js/2.aadc151e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "41a4eabb546362dfb98a",
+    "url": "/simple-todo-app/static/js/main.32ad7f23.chunk.js"
+  },
+  {
+    "revision": "854a73988d019f913f52",
+    "url": "/simple-todo-app/static/js/runtime-main.01792ca7.js"
+  }
+]);
